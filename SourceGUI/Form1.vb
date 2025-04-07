febaf57a3310f7dff2931256cb6880c64f555f41
TloJlb3oBaTeJlb <kachudov@gmail.com>
2025-04-07 09:20:45 +0300

    [TloJlb3oBaTeJlb] [code] Addwd a new dir for modules. Created Algorithm.vb with sorting algorithm

--- SourceGUI/Form1.vb
+++ SourceGUI/Form1.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.IO ' Для работы с файлами
+Imports SourceGUI.Algorithm
 
 Public Class Form1
 
@@ -103,50 +104,12 @@ Public Class Form1
         Dim arrayToSort As New List(Of Integer)(originalArray)
 
         ' Выполняем сортировку
-        sortedArray = QuickSort(arrayToSort)
+        sortedArray = Algorithm.QuickSort(arrayToSort)
 
         ' Отображаем результат
         DisplayArray(sortedArray, txtSortedArray)
         btnSave.Enabled = True ' Разрешаем сохранение
     End Sub
-
-    ' --- Реализация QuickSort ---
-    Private Function QuickSort(list As List(Of Integer)) As List(Of Integer)
-        If list Is Nothing OrElse list.Count <= 1 Then
-            Return list ' Базовый случай рекурсии
-        End If
-
-        Dim pivotIndex As Integer = list.Count \ 2 ' Взятие опорного элемента из середины
-        Dim pivotValue As Integer = list(pivotIndex)
-
-        Dim less As New List(Of Integer)()
-        Dim equal As New List(Of Integer)()
-        Dim greater As New List(Of Integer)()
-
-        ' Разделение на три части
-        For Each item As Integer In list
-            If item < pivotValue Then
-                less.Add(item)
-            ElseIf item = pivotValue Then
-                equal.Add(item)
-            Else ' item > pivotValue
-                greater.Add(item)
-            End If
-        Next
-
-        ' Рекурсивный вызов для меньших и больших частей
-        Dim sortedLess As List(Of Integer) = QuickSort(less)
-        Dim sortedGreater As List(Of Integer) = QuickSort(greater)
-
-        ' Объединение результатов
-        Dim result As New List(Of Integer)()
-        result.AddRange(sortedLess)
-        result.AddRange(equal)
-        result.AddRange(sortedGreater)
-
-        Return result
-    End Function
-    ' --- Конец QuickSort ---
 
     ' Кнопка "Сохранить отсортированный..."
     Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
